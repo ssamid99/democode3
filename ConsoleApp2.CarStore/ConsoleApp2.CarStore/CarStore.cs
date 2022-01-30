163ed002df511f1f159b52062edcdc834823919d
Samid <samidms@code.edu.az>
2022-01-30 12:52:02 +0400

    task problem

---
+++ ConsoleApp2.CarStore/ConsoleApp2.CarStore/CarStore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.CarStore
+{
+    class CarStore
+    {
+        public string brand;
+        public string model;
+        public string city;
+        public int graduitionYear;
+        public string banType;
+        public string color;
+        public double engine;
+        public int enginePower;
+        public string fuelType;
+        public int kmRate;
+        public string gearbox;
+        public string transmitter;
+        public string situation;
+        public double price;
+
+        public string GetInfo()
+        {
+            string output = $"{brand}\n" +
+                $"{model}\n" +
+                $"{city}\n" +
+                $"{graduitionYear}\n" +
+                $"{banType}\n" +
+                $"{color}\n" +
+                $"{engine}l \n" +
+                $"{enginePower} hp \n" +
+                $"{fuelType}\n" +
+                $"{kmRate} km \n" +
+                $"{gearbox}\n" +
+                $"{transmitter}\n" +
+                $"{situation}\n" +
+                $"{price}€";
+            return output;
+        }
+    }
+}
---
+++ ConsoleApp2.CarStore/ConsoleApp2.CarStore/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2.CarStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            
+            CarStore car1 = new CarStore();
+
+            car1.brand = "Bentley";
+            car1.model = "Flying Spur";
+            car1.city = "Baki";
+            car1.graduitionYear = 2021;
+            car1.banType = "Sedan";
+            car1.color = "Black";
+            car1.engine = 4.0;
+            car1.enginePower = 550;
+            car1.fuelType = "Gasoline";
+            car1.kmRate = 1770;
+            car1.gearbox = "Automatic";
+            car1.transmitter = "Full";
+            car1.situation = "New";
+            car1.price = 255.000;
+
+            CarStore car2 = new CarStore();
+
+
+            car1.brand = "Ford";
+            car1.model = "Flying Spur";
+            car1.city = "Baki";
+            car1.graduitionYear = 2021;
+            car1.banType = "Sedan";
+            car1.color = "Black";
+            car1.engine = 4.0;
+            car1.enginePower = 550;
+            car1.fuelType = "Gasoline";
+            car1.kmRate = 1770;
+            car1.gearbox = "Automatic";
+            car1.transmitter = "Full";
+            car1.situation = "New";
+            car1.price = 255.000;
+
+            CarStore[] cars = new CarStore[3];
+            cars[0] = car1;
+            cars[1] = car2;
+           
+
+            foreach (CarStore car in cars)
+            {
+                Console.WriteLine(car.GetInfo());
+
+            }
+        }
+    }
+}
